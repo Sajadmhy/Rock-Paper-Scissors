c3f50cb4750056b297c5cc82c41add16b9f44aa6
Sajadmhy <sajad.mahyaei@gmail.com>
2022-03-12 07:34:48 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // function computerPlay randomly return either "rock", "paper" or "scissors"
-// function playRound takes two parameters and then declares the winner
 
 function computerPlay () {
     let number = Math.floor(Math.random()*3);
@@ -11,3 +10,32 @@ function computerPlay () {
         return `scissors`;
     }
 }
+
+// function playRound takes two parameters and then declares the winner
+
+function playRound (playerSel, computerSel) {
+    if (playerSel == `rock` && computerSel == `rock`) {
+        return `It's a tie!`;
+    } else if (playerSel == `rock` && computerSel == `paper`) {
+        return `You Lose`;
+}   else if (playerSel == `rock` && computerSel == `scissors`) {
+    return `You win`;
+}   else if (playerSel == `paper` && computerSel == `rock`) {
+    return `You win`;
+}   else if (playerSel == `paper` && computerSel == `scissors`) {
+    return `You Lose`;
+}   else if (playerSel == `paper` && computerSel == `paper`) {
+    return `It's a tie!`;
+}   else if (playerSel == `scissors` && computerSel == `scissors`) {
+    return `It's a tie!`;
+}   else if (playerSel == `scissors` && computerSel == `paper`) {
+    return `You win`;
+}   else if (playerSel == `scissors` && computerSel == `rock`) {
+    return `You Lose`;
+}
+}
+
+
+const playerSel = prompt(`let's play rock, paper, scissors`).toLowerCase();
+const computerSel = computerPlay();
+console.log(playRound(playerSel, computerSel));
